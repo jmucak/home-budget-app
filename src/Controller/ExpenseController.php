d64a3dd7fcce93ab5d0793c9b8f31a11a4f9f6b8
josip <josip.mucak@bornfight.com>
2023-11-13 16:52:10 +0100

    add expense crud

--- src/Controller/ExpenseController.php
+++ src/Controller/ExpenseController.php
@@ -4,31 +4,117 @@ namespace App\Controller;
 
 use App\Entity\Expense;
 use App\Entity\ExpenseCategory;
+use App\Repository\ExpenseCategoryRepository;
+use App\Repository\ExpenseRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/api', name: 'expense_api')]
 class ExpenseController extends AbstractController
 {
-    #[Route('/expense', name: 'app_expense')]
-    public function index(EntityManagerInterface $entityManager): Response
+    #[Route('/expense', name: 'app_expense', methods: ['GET'])]
+    public function index(ExpenseRepository $expenseRepository): Response
     {
-//        $category = new ExpenseCategory();
-//        $category->setName('Food');
-//
-//        $expense = new Expense();
-//        $expense->setAmount('11.22');
-//        $expense->setDescription('Food supplies');
-//        $expense->setCreated(new DateTime());
-//        $expense->setCategory($category);
-//
-//        $entityManager->persist($expense);
-//        $entityManager->flush();
+        $expenses = $expenseRepository->findAll();
 
-        return $this->render('expense/index.html.twig', [
-            'controller_name' => 'ExpenseController',
-        ]);
+        if (empty($expenses)) {
+            return $this->json(array(
+                'message' => 'No expenses',
+            ));
+        }
+
+        $data = array();
+        foreach ($expenses as $expense) {
+            $data[] = array(
+                'id'          => $expense->getId(),
+                'description' => $expense->getDescription(),
+                'amount'      => $expense->getAmount(),
+                'category'    => $expense->getCategory(),
+                'created'     => $expense->getCreated(),
+            );
+        }
+
+        return $this->json($data);
+    }
+
+    #[Route('/expense', name: 'app_expense_add', methods: ['POST'])]
+    public function addExpense(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ExpenseCategoryRepository $expenseCategoryRepository
+    ): Response {
+
+        $expense  = new Expense();
+        $category = $expenseCategoryRepository->find($request->get('category'));
+        $expense->setCreated(new DateTime());
+        $expense->setDescription($request->get('description'));
+        $expense->setCategory($category);
+        $expense->setAmount($request->get('amount'));
+
+        $entityManager->persist($expense);
+        $entityManager->flush();
+
+        return $this->json(array(
+            'message' => 'Expense '.$expense->getDescription().' added to database',
+        ));
+    }
+
+    #[Route('/expense/{id}', name: 'app_expense_edit', methods: ['PUT'])]
+    public function updateExpense(
+        int $id,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ExpenseRepository $expenseRepository,
+        ExpenseCategoryRepository $expenseCategoryRepository
+    ): Response {
+        $expense = $expenseRepository->find($id);
+
+        if ( ! empty($request->get('description'))) {
+            $expense->setDescription($request->get('description'));
+        }
+
+        if ( ! empty($request->get('amount'))) {
+            $expense->setAmount($request->get('amount'));
+        }
+
+        if ( ! empty($request->get('category'))) {
+            $category = $expenseCategoryRepository->find($request->get('category'));
+            $expense->setCategory($category);
+        }
+
+        $entityManager->persist($expense);
+        $entityManager->flush();
+
+        return $this->json(array(
+            'message' => 'Expense successfully updated',
+        ));
+    }
+
+    #[Route('/expense/{id}', name: 'app_expense_delete', methods: ['DELETE'])]
+    public function deleteExpense(
+        int $id,
+        ExpenseRepository $expenseRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $expense = $expenseRepository->find($id);
+
+        if ( ! $expense) {
+            return $this->json(array(
+                'status'  => 404,
+                'message' => 'Post not found',
+            ));
+        }
+
+        $entityManager->remove($expense);
+        $entityManager->flush();
+
+        return $this->json(array(
+            'status'  => 200,
+            'message' => 'Expense deleted successfully',
+        ));
     }
 }
