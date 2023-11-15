11fda398e1be2887fcad0e7b18228ad7dd050340
josip <josip.mucak@bornfight.com>
2023-11-15 13:06:37 +0100

    update OA parameters for data aggregation

--- src/Controller/DataAggregationController.php
+++ src/Controller/DataAggregationController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Repository\ExpenseRepository;
+use Nelmio\ApiDocBundle\Annotation\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use OpenApi\Attributes as OA;
 
 #[Route('/api/data')]
 #[AsController]
@@ -16,10 +18,23 @@ class DataAggregationController extends AbstractController
 {
     #[Route('/aggregation', name: 'app_data_aggregation', methods: 'GET')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[OA\Response(
+        response: 200,
+        description: 'Get total money spent',
+    )]
+    #[OA\Parameter(
+        name: 'date',
+        description: 'The field used to get data by date',
+        in: 'query',
+        required: true,
+        schema: new OA\Schema(type: 'string', enum: ['month', 'quarter', 'year'])
+    )]
+    #[OA\Tag(name: 'Data aggregation')]
+    #[Security(name: 'Bearer')]
     public function getMoneySpent(
         ExpenseRepository $expenseRepository,
         Request $request
-    ): Response {
+    ): JsonResponse {
         $expenses = $expenseRepository->findAllByUser($this->getUser(), [
             'date' => $request->get('date'),
         ]);
@@ -30,7 +45,7 @@ class DataAggregationController extends AbstractController
         }
 
         return $this->json([
-            'total' => $total,
+            'total' => number_format($total, 2),
         ]);
     }
 }
--- src/Controller/ExpenseController.php
+++ src/Controller/ExpenseController.php
@@ -12,7 +12,6 @@ use Nelmio\ApiDocBundle\Annotation\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
